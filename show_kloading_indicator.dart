49043259b9de8e38427fefaf92adf671f9aae72d
Kevin Saucedo <developer2@sfperusac.com>
2023-09-09 12:48:21 -0500

    dialos to show spinners

---
+++ show_kloading_indicator.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+/// returns a function with which we can finish the loading process.
+Future<void Function()> showKLoadingIndicatorWithMessage(
+  BuildContext context, {
+  String message = "Loading Please Wait...",
+  TextStyle? textStyle,
+}) async {
+  textStyle ??= const TextStyle(color: Colors.black38, fontSize: 14, height: 1.2);
+  void Function()? clouser;
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      clouser = () {
+        if (context.mounted) Navigator.of(context).pop(null);
+      };
+      return WillPopScope(
+        onWillPop: () async => false,
+        child: AlertDialog(
+          content: Row(
+            children: [
+              const CircularProgressIndicator(),
+              const SizedBox(width: 22.0),
+              Expanded(
+                child: Text(
+                  message,
+                  style: textStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+  while (clouser == null) {
+    // wait until the closure variable becomes non-null.
+    await Future.delayed(const Duration(milliseconds: 1));
+  }
+  return clouser ?? () => {};
+}
+
+Future<void Function()> showKLoadingIndicator<T>(BuildContext context) async {
+  void Function()? clouser;
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      clouser = () {
+        if (context.mounted) Navigator.of(context).pop(null);
+      };
+      return WillPopScope(
+        onWillPop: () async => false,
+        child: const AlertDialog(
+          shadowColor: Colors.transparent,
+          backgroundColor: Colors.transparent,
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(color: Colors.white),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+  while (clouser == null) {
+    // wait until the closure variable becomes non-null.
+    await Future.delayed(const Duration(milliseconds: 1));
+  }
+  return clouser ?? () => {};
+}
